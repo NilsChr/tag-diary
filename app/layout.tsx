7b64bb6406bc2961b575375918b16394920dc1f6
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2026-07-28 02:15:34 +0200

    removed zooming

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,10 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: "#0f172a",
   viewportFit: "cover",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
